1f8e0020544a1f33aaa781309c731f15fa20894f
GerzsenyiRoland <103255961+GerzsenyiRoland@users.noreply.github.com>
2022-05-10 19:10:58 +0200

    first commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.SortedMap;;
+
+//https://github.com/GerzsenyiRoland/NagyHazi.git
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    public void main(String agrs[]){
+        ArrayList<Ital> italok = new ArrayList<>();
+        SortedMap<String,ArrayList<Ital>> receptek;
+        while(true){
+            System.out.println("Alma");
+        }
+    }
+}
